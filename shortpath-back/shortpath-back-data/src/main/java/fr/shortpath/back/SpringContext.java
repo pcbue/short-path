b7413e0b239749c4f2a0bf537cbe400eaa330f98
pcbue <pierrechristophe.bue@gmail.com>
2016-02-12 15:56:20 +0100

    suggest

--- shortpath-back/shortpath-back-data/src/main/java/fr/shortpath/back/SpringContext.java
+++ shortpath-back/shortpath-back-data/src/main/java/fr/shortpath/back/SpringContext.java
@@ -1,9 +1,11 @@
 package fr.shortpath.back;
 
 import fr.shortpath.back.repository.ProducerRepository;
+import fr.shortpath.back.repository.ProducerSuggester;
 import fr.shortpath.back.service.ProducerService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
@@ -13,5 +15,10 @@ public class SpringContext {
     @Bean
     public ProducerService producerService(ProducerRepository repository) {
         return new ProducerService(repository);
+    }
+
+    @Bean
+    public ProducerSuggester producerSuggester(ElasticsearchTemplate elasticsearchTemplate) {
+        return new ProducerSuggester(elasticsearchTemplate);
     }
 }
