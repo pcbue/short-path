0f06078d8df6cc47c09abe6cf85fca7983373d21
pcbue <pierrechristophe.bue@gmail.com>
2016-02-12 10:55:37 +0100

    es as an external resource

--- shortpath-back/shortpath-back-application/src/main/java/fr/shortpath/application/ShortpathApplication.java
+++ shortpath-back/shortpath-back-application/src/main/java/fr/shortpath/application/ShortpathApplication.java
@@ -1,11 +1,8 @@
 package fr.shortpath.application;
 
-import org.elasticsearch.node.NodeBuilder;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 
 @SpringBootApplication(
     scanBasePackages = {"fr.shortpath.*"}
@@ -15,13 +12,6 @@ public class ShortpathApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(ShortpathApplication.class);
-    }
-
-    @Bean
-    public ElasticsearchTemplate elasticsearchTemplate() {
-        return new ElasticsearchTemplate(
-            NodeBuilder.nodeBuilder().client(true).node().client()
-        );
     }
 
     public static void main(String... args) {
